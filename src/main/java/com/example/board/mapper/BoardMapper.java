22c8563b39048819e2ee3e4c7c2ca2002a45222c
SANGHYEOK <springbean3@outlook.kr>
2021-07-20 00:21:58 +0900

    2021.7.19. 4.3 전까지

---
+++ src/main/java/com/example/board/mapper/BoardMapper.java
@@ -0,0 +1,11 @@
+package com.example.board.mapper;
+
+import com.example.board.board.dto.BoardDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    List<BoardDto> selectBoardList() throws Exception;
+}
---
+++ src/main/java/com/example/board/service/BoardServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.board.service;
+
+import com.example.board.board.dto.BoardDto;
+import com.example.board.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+    @Autowired
+    private BoardMapper boardMapper;
+    @Override
+    public List<BoardDto> selectBoardList() throws Exception {
+        return boardMapper.selectBoardList();
+    }
+}
